6e2aaf1c2513c4183a4d8feeb2b7d278281259bf
嘿鹰 <hhhhhy1747@outlook.com>
2024-12-10 19:04:42 +0800

    优化载入

--- src/main/kotlin/kim/azure/kether/AzureKetherLite.kt
+++ src/main/kotlin/kim/azure/kether/AzureKetherLite.kt
@@ -1,12 +1,34 @@
 package kim.azure.kether
 
+import io.rokuko.azureflow.features.addons.Addon
+import io.rokuko.azureflow.features.addons.AddonService
 import kim.azure.kether.resolver.KetherResolver
 import taboolib.common.platform.Plugin
 import taboolib.common.platform.function.info
+import taboolib.common.platform.function.pluginId
 
-object AzureKetherLite : Plugin() {
+object AzureKetherLite : Plugin(), Addon {
+
+    override fun onLoad() {
+        AddonService.register(this)
+    }
 
     override fun onEnable() {
         info("Successfully running AzureKetherLite!")
+    }
+
+    override fun onDisable() {
+        AddonService.unRegister(this)
+    }
+
+    override fun getName(): String {
+        return pluginId
+    }
+
+    override fun register() {
+        KetherResolver.register()
+    }
+
+    override fun unRegister() {
     }
 }
--- src/main/kotlin/kim/azure/kether/resolver/KetherResolver.kt
+++ src/main/kotlin/kim/azure/kether/resolver/KetherResolver.kt
@@ -1,24 +1,28 @@
 package kim.azure.kether.resolver
 
+import io.rokuko.azureflow.AzureFlow
 import io.rokuko.azureflow.features.action.ActionBindings
 import io.rokuko.azureflow.features.action.AzureFlowActionableObject
 import io.rokuko.azureflow.features.resolver.provided.PrefixSupportResolver
 import kim.azure.kether.evalKether
-import taboolib.common.LifeCycle
-import taboolib.common.platform.Awake
+import taboolib.common.platform.function.pluginId
 
-object KetherResolver: PrefixSupportResolver("ke") {
+object KetherResolver : PrefixSupportResolver("ke") {
+
+    val actionName: String
+        get() = "ke"
+
     override fun doAction(
         args: String,
         actionBindings: ActionBindings,
         actionableObject: AzureFlowActionableObject
     ): Boolean {
-        args.evalKether(actionableObject.player)
+        args.evalKether(actionableObject.player, mapOf("item-amount" to actionableObject.itemStack))
         return true
     }
 
-    @Awake(LifeCycle.ENABLE)
-    fun init() {
-        register()
+    override fun register() {
+        AzureFlow.print("正在加载注册附属动作 [ $pluginId ( &f&l$actionName&7 ) ] 中..")
+        AzureFlow.print("成功注册附属 [ $pluginId ( &f&l$actionName&7 ) ]")
     }
 }
