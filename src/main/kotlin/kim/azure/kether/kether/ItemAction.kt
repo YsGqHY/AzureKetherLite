82d0ee6969377fd14a5f3d265e7d836b496eedfb
嘿鹰 <hhhhhy1747@outlook.com>
2024-12-10 20:37:22 +0800

    1.1.1
    新增Kether

--- src/main/kotlin/kim/azure/kether/kether/ItemAction.kt
+++ src/main/kotlin/kim/azure/kether/kether/ItemAction.kt
@@ -7,7 +7,9 @@ import taboolib.common5.cint
 import taboolib.module.kether.KetherParser
 import taboolib.module.kether.ScriptFrame
 import taboolib.module.kether.combinationParser
+import taboolib.module.nms.getName
 import taboolib.platform.util.modifyMeta
+import kotlin.jvm.optionals.getOrNull
 
 /**
  * 扣除触发的物品数量
@@ -18,6 +20,12 @@ import taboolib.platform.util.modifyMeta
  *
  * 触发的物品重命名
  * azure-item rename "666 66 6"
+ *
+ * 获取原始物品名
+ * azure-item name
+ *
+ * 获取原始物品 Lore, 列表类型, 可用 element 0 of azure-item lore 获取第一行 lore
+ * azure-item lore
  */
 object ItemAction {
     /**
@@ -31,24 +39,32 @@ object ItemAction {
     fun put() = combinationParser {
         it.group(
             text(),
-            text()
+            text().optional()
         ).apply(it) { oper, token ->
             now {
                 when (oper) {
                     "take", "remove", "consume" -> {
-                        getItemStack().amount -= token.cint
+                        getItemStack().amount -= token.getOrNull().cint
                     }
 
                     "give", "add" -> {
                         val item = getItemStack().clone().also { i -> i.amount = 1 }
                         val inv = getBukkitPlayer().inventory
-                        repeat(token.cint) {
+                        repeat(token.getOrNull().cint) {
                             inv.addItem(item)
                         }
                     }
 
                     "rename" -> {
-                        getItemStack().modifyMeta<ItemMeta> { setDisplayName(token) }
+                        getItemStack().modifyMeta<ItemMeta> { setDisplayName(token.getOrNull() ?: "null") }
+                    }
+
+                    "name" -> {
+                        getItemStack().getName(getBukkitPlayer())
+                    }
+
+                    "lore" -> {
+                        getItemStack().itemMeta?.lore ?: emptyList<String>()
                     }
 
                     else -> {
